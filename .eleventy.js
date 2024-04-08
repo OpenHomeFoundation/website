3760031841d86c35487c685ab5a01faa8137cad3
Paul Bottein <paul.bottein@gmail.com>
2024-04-08 09:57:27 +0200

    Add building script

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,23 @@
-module.exports = function () {
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addWatchTarget("./src/assets");
+
+  // 	--------------------- Custom Template Languages ---------------------
+  eleventyConfig.addPlugin(
+    require("./config/css-config.js")
+  );
+
+  // 	--------------------- Passthrough File Copy -----------------------
+  eleventyConfig.addPassthroughCopy("src/assets/fonts/");
+  eleventyConfig.addPassthroughCopy({
+    "src/assets/favicon/*": "/",
+  });
+
   return {
     dir: {
       input: "src",
       output: "dist",
     },
     htmlTemplateEngine: "liquid",
+    pathPrefix: "/",
   };
 };
