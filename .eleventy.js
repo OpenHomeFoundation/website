523e15e14797ad515d48d146e82693509282000a
Paul Bottein <paul.bottein@gmail.com>
2024-04-08 15:19:35 +0200

    Add navbar

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,12 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addWatchTarget("./src/assets");
 
   // 	--------------------- Custom Template Languages ---------------------
-  eleventyConfig.addPlugin(
-    require("./config/css-config.js")
-  );
+  eleventyConfig.addPlugin(require("./config/css-config.js"));
+  eleventyConfig.addPlugin(require('./config/js-config.js'));
 
   // 	--------------------- Passthrough File Copy -----------------------
   eleventyConfig.addPassthroughCopy("src/assets/fonts/");
+  eleventyConfig.addPassthroughCopy("src/assets/images/");
   eleventyConfig.addPassthroughCopy({
     "src/assets/favicon/*": "/",
   });
---
+++ src/assets/js/nav-menu.js
@@ -0,0 +1,30 @@
+const button = document.querySelector("#navbar-menu-button");
+const menu = document.querySelector("#navbar-menu");
+
+button.addEventListener("click", (e) => {
+  const isOpen = button.getAttribute("aria-expanded") === "true";
+  setMenuOpen(!isOpen);
+});
+
+const setMenuOpen = (open) => {
+  button.setAttribute("aria-expanded", open);
+  menu.classList.toggle("expanded", open);
+};
+
+const closeMenu = () => {
+  setMenuOpen(false);
+};
+
+menu.addEventListener("keyup", (e) => {
+  if (e.code === "Escape") {
+    closeMenu();
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const isClickInsideElement =
+    menu.contains(e.target) || button.contains(e.target);
+  if (!isClickInsideElement) {
+    closeMenu();
+  }
+});
