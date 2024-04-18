8b01df1e0b28a8fca242e977b5bd3f47b911b887
Paul Bottein <paul.bottein@gmail.com>
2024-04-18 18:44:36 +0200

    Add 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,7 @@ module.exports = (eleventyConfig) => {
     "src/assets/fonts/": "assets/fonts/",
     "src/assets/images/": "assets/images/",
     "src/assets/favicon/*": "/",
+    "src/robots.txt": "/robots.txt",
   })
 
   //   --------------------- Passthrough CSS -----------------------
