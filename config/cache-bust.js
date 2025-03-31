b17122599f4c146783b12c26c5601a31e0b4d3ae
Darren Griffin <darren.griffin@live.co.uk>
2025-03-31 16:44:41 +0000

    Add cache busting to remove need for Cloudflare purging

---
+++ config/cache-bust.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addFilter("cacheKey", (url) => {
+        const [urlPart, paramPart] = url.split("?");
+        const params = new URLSearchParams(paramPart || "");
+        const relativeUrl = "./dist/" + ((urlPart.charAt(0) == "/") ? urlPart.substring(1) : urlPart);
+
+        try {
+
+            const fileStats = fs.statSync(relativeUrl);
+
+            const dateTimeModified = Math.floor(fileStats.mtime.getTime() / 1000);
+            params.set("v", dateTimeModified);
+
+        } catch (error) { }
+
+        if (params.has("v")) {
+            return `${urlPart}?${params}`;
+        }
+
+        return url;
+    });
+}
