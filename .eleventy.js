8aa8de2fc4fc1dbbd8970ee61117114e35bef998
Joakim Sørensen <joasoe@gmail.com>
2025-06-04 13:51:00 +0100

    Implement cache buster for css and js asset files (#19)
    * Implement cache buster for css and js asset files
    
    * Better handling for node_module copies

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const { resolve } = require("path");
+const { statSync, existsSync } = require("fs");
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addWatchTarget("./src/assets");
 
@@ -17,6 +20,34 @@ module.exports = (eleventyConfig) => {
   //   --------------------- Passthrough CSS -----------------------
   eleventyConfig.addPassthroughCopy({
     "node_modules/swiper/swiper-bundle.min.css": "assets/swiper-bundle.min.css",
+  });
+
+  eleventyConfig.addTransform("cache-buster", function (content, outputPath) {
+    const assetsInputDir = resolve(eleventyConfig.dir.input, "assets");
+    const assetsOutputDir = resolve(eleventyConfig.dir.output, "assets");
+    if (outputPath.endsWith(".html")) {
+      return content.replace(
+        /="\/assets\/([^"]+\.(css|js))"/g,
+        function (_, matcher) {
+          const filepath = matcher.split("?")[0];
+          let timestamp
+
+          if (existsSync(resolve(assetsInputDir, filepath))) {
+            // These are our source files, so we use their modification time
+            timestamp = statSync(resolve(assetsInputDir, filepath)).mtime.getTime();
+          } else if (existsSync(resolve(assetsOutputDir, filepath))) {
+            // In cases where we are getting the output files from other places (like node_modules)
+            // we use the output directory's modification time.
+            timestamp = statSync(resolve(assetsOutputDir, filepath)).mtime.getTime();
+          } else {
+            throw new Error(`File not found: ${filepath}`);
+          }
+
+          return `="/assets/${filepath}?v=${timestamp}"`;
+        }
+      );
+    }
+    return content;
   });
 
   return {
